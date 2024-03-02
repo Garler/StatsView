51baa541d55d2010fd1d3069963a40a7b0991375
Igor Repin <repin_iv@vk.com>
2024-03-02 21:49:45 +0500

    lesson code

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.netology.statsview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.animation.Animation
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.ui.StatsView
 
@@ -9,10 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<StatsView>(R.id.StatsView).data = listOf(
+        val view = findViewById<StatsView>(R.id.StatsView)
+        view.data = listOf(
 //            500F, 500F, 500F, 500F  // Задача. Smart StatsView
             0.25F, 0.25F, 0.25F,  //Задача. Not Filled
 
         )
+        val textView = findViewById<TextView>(R.id.label)
+        view.startAnimation(
+            android.view.animation.AnimationUtils.loadAnimation(this, R.anim.animation)
+                .apply {
+                    setAnimationListener(object : Animation.AnimationListener {
+                        @SuppressLint("SetTextI18n")
+                        override fun onAnimationStart(animation: Animation?) {
+                            textView.text = "started"
+                        }
+                        @SuppressLint("SetTextI18n")
+                        override fun onAnimationEnd(animation: Animation?) {
+                            textView.text = "ended"
+                        }
+                        @SuppressLint("SetTextI18n")
+                        override fun onAnimationRepeat(animation: Animation?) {
+                            textView.text = "repeat"
+                        }
+                    }
+                    )
+                })
     }
 }
+
